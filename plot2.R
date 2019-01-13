69986a6dd0c2ec619bbf2f3374415cf88f87bc76
jemlunar <lunar_jermaine@yahoo.com>
2019-01-13 11:22:46 +0800

    Add plot1 to plot4 R files and png files

---
+++ plot2.R
@@ -0,0 +1,18 @@
+##Reading and Formatting the Data to be Used
+
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+data <- subset(data,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+data <- data[complete.cases(data),]
+dateTime <- paste(data$Date, data$Time)
+dateTime <- setNames(dateTime, "DateTime")
+data <- data[ ,!(names(data) %in% c("Date","Time"))]
+data <- cbind(dateTime, data)
+data$dateTime <- as.POSIXct(dateTime)
+
+## Create Plot 2
+plot(data$Global_active_power~data$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+
+## Save file and close device
+dev.copy(png,"plot2.png", width=480, height=480)
+dev.off()
